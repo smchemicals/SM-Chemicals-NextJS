c05f51859a0056a0e861f11c1feecf301e16e602
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2023-10-27 22:43:29 +0200

    Implemented basic header

---
+++ lib/data.ts
@@ -0,0 +1,29 @@
+export const smcText = [
+	{
+		name: "SMC",
+		hash: "#home",
+	},
+] as const;
+
+export const links = [
+	{
+		name: "Home",
+		hash: "#home",
+	},
+	{
+		name: "About Us",
+		hash: "#aboutus",
+	},
+	{
+		name: "Products List",
+		hash: "#productslist",
+	},
+	{
+		name: "Contact Us",
+		hash: "#contactus",
+	},
+	{
+		name: "Career",
+		hash: "#career",
+	},
+] as const;

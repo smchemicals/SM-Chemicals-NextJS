415c698abc2deb12ecc2156161c169151770ecca
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2023-10-30 15:20:13 +0100

    Adding about us section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import AboutUs from "@/components/aboutus";
 import Intro from "@/components/intro";
 
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center px-4">
 			<Intro />
+			<AboutUs />
 		</main>
 	);
 }
